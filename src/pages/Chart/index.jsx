8e5368f3d9900c00db483094df17b42715a45f9e
yinxiaoli <yinxiaoli@guazi.com>
2019-03-26 18:09:35 +0800

    update

--- src/pages/Chart/index.jsx
+++ src/pages/Chart/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import ReactEcharts from 'echarts-for-react';
 import mapToProps from './mapping';
 import styles from './index.less';
 
@@ -19,9 +20,28 @@ export default class Chart extends React.PureComponent {
 
     render() {
         // const { } = this.props;
+        const option = {
+            title: {
+                text: '公共地点人流量云监管数据图表',
+            },
+            // width: '100%',
+            xAxis: {
+                type: 'category',
+                boundaryGap: false,
+                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+            },
+            yAxis: {
+                type: 'value',
+            },
+            series: [{
+                data: [820, 932, 901, 934, 1290, 1330, 1320],
+                type: 'line',
+                areaStyle: {},
+            }],
+        };
         return (
             <div className={styles.chartWrapper}>
-                哈哈哈
+                <ReactEcharts option={option} style={{ width: '100%' }}/>
             </div>
         );
     }
